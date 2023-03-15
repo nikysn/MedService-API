93b11997d9faed6c8a34544504a80257f9cbd425
Vadim <nikysn@yandex.ru>
2023-03-15 09:44:09 +0300

    - Добавил проект MedServiceTests

--- MedServiceAPI/MedServiceAPI/Model/Doctor.cs
+++ MedServiceAPI/MedServiceAPI/Model/Doctor.cs
@@ -1,11 +1,4 @@
-﻿using MedServiceAPI.Model;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Text.Json.Serialization;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MedServiceAPI.Model
 {
@@ -51,36 +44,6 @@ namespace MedServiceAPI.Model
                 Schedule[day] = new List<TimeSpan>();
                 Schedule[day].AddRange(timeSpans);
             }
-        }
-
-        public List<TimeSpan> GetFreeAppointmentTimesDate(DateTime date)
-        {
-            var occupiedTimes = AppointmentDate //AppointmentDate - это коллекция объектов AppointmentDate,
-                                                //которые содержат информацию о занятых временах.
-                .FirstOrDefault(ad => ad.Date == date.Date)?.AppointmentTimes //это метод Linq, который ищет первый элемент в коллекции AppointmentDate,
-                                                                              //который соответствует условию, заданному в скобках.
-                                                                              //В этом случае мы ищем объект AppointmentDate,
-                                                                              //у которого свойство Date соответствует указанной дате date.Date.
-                                                                              //
-                                                                              //?.AppointmentTimes - это оператор ?. (null-условный оператор),
-                                                                              //который позволяет проверить, является ли найденный объект AppointmentDate равным null,
-                                                                              //прежде чем вызывать свойство AppointmentTimes.
-                                                                              //Если объект AppointmentDate равен null, оператор ?. вернет null и следующие методы Linq не будут вызваны.
-
-                .Where(at => at.Time != null) // это метод Linq, который фильтрует список времен AppointmentTimes,
-                                              // оставляя только те, у которых свойство Time не равно null.
-                .ToList();
-            // Таким образом, в результате выполнения этого кода мы получаем список занятых времен для указанной даты date.Date,
-            // который не содержит элементов со значением null в свойстве Time.
-
-            var freeTimes = Schedule[date.DayOfWeek]  //это получение списка времен для указанного дня недели на основе расписания Schedule.
-                                                      //Это означает, что мы получаем список времен для понедельника, вторника, среды и т.д.,
-                                                      //в зависимости от значения свойства date.DayOfWeek.
-                .Where(t => !occupiedTimes.Any(ot => ot.Time == t))
-                //.Select(time => new AppointmentTime (time, 0))
-                .ToList();
-
-            return freeTimes;
         }
     }
 }
